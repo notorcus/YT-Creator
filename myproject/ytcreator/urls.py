2680a7336823bfa918914f1dc8cefe8153bf406a
Akki <akshat.kr12090@gmail.com>
2023-08-07 17:41:29 -0400

    Now files are transcribed from frontend
    Project structure is created from API call and then files are also transcribed and saved in the correct folder. Also download_video renamed to process_video

--- myproject/ytcreator/urls.py
+++ myproject/ytcreator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import download_video
+from .views import process_video
 
 urlpatterns = [
-    path('api/download_video', download_video),
+    path('api/process_video', process_video),
 ]
